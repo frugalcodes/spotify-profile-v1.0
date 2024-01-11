5407d2cb1ae583928ead57eee78faa62596cddd8
frugalcodes <samueligwe63@gmail.com>
2024-01-11 16:28:45 +0100

    fix proxy

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-let REDIRECT_URI = process.env.REDIRECT_URI || 'https://spotify-profile-v1-0.onrender.com/callback';
-let FRONTEND_URI = process.env.FRONTEND_URI || 'https://spotify-profile-v1-0.onrender.com';
+let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
+let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
 const PORT = process.env.PORT || 8888;
 
 if (process.env.NODE_ENV !== 'production') {
-  REDIRECT_URI = 'https://spotify-profile-v1-0.onrender.com/callback';
-  FRONTEND_URI = 'https://spotify-profile-v1-0.onrender.com';
+  REDIRECT_URI = 'http://localhost:8888/callback';
+  FRONTEND_URI = 'http://localhost:3000';
 }
 
 const express = require('express');
